02f07df88edcddaeb9414a5790a0c7a83e65da4f
Aryan <ark.opash@gamil.com>
2026-03-16 15:40:22 +0530

    routes and middleware added

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import userRoutes from "./routes/user.routes.js";
+import { requestLogger } from './middlewares/logger.middleware.js';
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/users", userRoutes);
+
+app.use(requestLogger);
 
 export default app;
